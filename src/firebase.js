01248dce892452f8b844a724fa36e621096b8c13
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-11-16 18:45:08 +0600

    initial commit and clean app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpbXNUv2WdOBnLatoCrkdTzvIkLghg5Uc",
+    authDomain: "whatsapp-clone-kamrulsh.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-kamrulsh.firebaseio.com",
+    projectId: "whatsapp-clone-kamrulsh",
+    storageBucket: "whatsapp-clone-kamrulsh.appspot.com",
+    messagingSenderId: "462692407068",
+    appId: "1:462692407068:web:f86608b5fef08c7c2c4cd9",
+};
