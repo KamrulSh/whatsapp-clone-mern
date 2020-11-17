8ea2924c7b62efa5635761f3b510f97db52c24fe
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-11-17 10:16:02 +0600

    design and style sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
     return (
         <div className="app">
-            <h1>🚀 This is whatsapp clone using mern 🔥</h1>
+            <div className="app__body">
+                <Sidebar />
+                <Chat />
+            </div>
         </div>
     );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__headerLeft">
+                    <Avatar src="https://cutewallpaper.org/21/kamrul-islam/29578632911801464529123.jpg" />
+                </div>
+
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon color="action" />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input type="text" placeholder="Search or start new chat" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
