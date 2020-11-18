e4926aea931a64ce2c316f1afa5330f67641ea96
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-11-19 02:05:00 +0600

    send message from input

--- whatsapp-frontend/src/Chat.js
+++ whatsapp-frontend/src/Chat.js
@@ -6,10 +6,24 @@ import {
     MoreVert,
     SearchOutlined,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
+import axios from "./axios";
 
 function Chat({ messages }) {
+    const [input, setInput] = useState("");
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        await axios.post("/messages/new", {
+            message: input,
+            name: "Kamrul",
+            timestamp: "just now",
+            received: false,
+        });
+        setInput("");
+    };
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -51,8 +65,15 @@ function Chat({ messages }) {
             <div className="chat__footer">
                 <EmojiEmotionsOutlined />
                 <form action="">
-                    <input type="text" placeholder="Type a msg" />
-                    <button type="submit">Send</button>
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        type="text"
+                        placeholder="Type a msg"
+                    />
+                    <button onClick={sendMessage} type="submit">
+                        Send
+                    </button>
                 </form>
                 <MicOutlined />
             </div>
