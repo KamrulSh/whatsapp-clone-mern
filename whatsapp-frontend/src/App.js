6dee9cf6be88f8b28ccb614579ea78369137cb66
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-11-19 01:20:18 +0600

    rendering realtime data

--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
         <div className="app">
             <div className="app__body">
                 <Sidebar />
-                <Chat />
+                <Chat messages={messages} />
             </div>
         </div>
     );
--- whatsapp-frontend/src/Chat.js
+++ whatsapp-frontend/src/Chat.js
@@ -9,7 +9,7 @@ import {
 import React from "react";
 import "./Chat.css";
 
-function Chat() {
+function Chat({ messages }) {
     return (
         <div className="chat">
             <div className="chat__header">
@@ -33,21 +33,19 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message">
-                    <span className="chat__name">shahin</span>
-                    This is sender msg
-                    <span className="chat__timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat__message chat__receiver">
-                    <span className="chat__name">shahin</span>
-                    This is receiver msg
-                    <span className="chat__timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                {messages.map((message) => (
+                    <p
+                        className={`chat__message ${
+                            message.received && "chat__receiver"
+                        }`}
+                    >
+                        <span className="chat__name">{message.name}</span>
+                        {message.message}
+                        <span className="chat__timestamp">
+                            {message.timestamp}
+                        </span>
+                    </p>
+                ))}
             </div>
 
             <div className="chat__footer">
